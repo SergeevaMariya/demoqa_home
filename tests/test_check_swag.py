ecef3e3184585a02b84bb3ec3d00295619526d5b
mari <mozhno_est@mail.ru>
2023-03-17 18:27:41 +0300

    addf

---
+++ tests/test_check_swag.py
@@ -0,0 +1,17 @@
+from pages.swag_lab import SwagLabs
+
+def test_swag(browser):
+    testswag = SwagLabs(browser)
+    testswag.vizit()
+    testswag.exist_icon()
+
+
+def test_swag2(browser):
+    testswag = SwagLabs(browser)
+    testswag.vizit()
+    testswag.find_element('#user-name')
+
+def tast_swag3(browser):
+    testswag = SwagLabs(browser)
+    testswag.vizit()
+    testswag.find_element('#password')
